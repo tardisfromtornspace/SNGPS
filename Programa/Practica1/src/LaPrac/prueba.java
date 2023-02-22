6f1d2159657e7f6384d5c8f1949f12057d84ee9f
tardisfromtornspace <andromedavirgoa@gmail.com>
2023-02-22 16:58:28 +0100

    Repo SNGPS

---
+++ Programa/Practica1/src/LaPrac/prueba.java
@@ -0,0 +1,16 @@
+package LaPrac;
+
+import java.util.EventListener;
+// TO-DO añadir el plugin de lectura de COM
+
+public class prueba {
+	
+	public prueba() {
+		EventListener cosa = new EventListener();
+	}
+	// TO-DO AÑADIR PATRON OBSERVER al puerto serie
+	
+	public static void main() {
+		
+	}
+}
